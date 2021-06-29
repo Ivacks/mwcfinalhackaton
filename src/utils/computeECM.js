59df1851f4bb542228da1555037d9558cb87ce53
rovilram <robertovillaresrama@gmail.com>
2021-06-29 12:59:27 +0200

    computeECM endpoint

--- src/utils/computeECM.js
+++ src/utils/computeECM.js
@@ -6,8 +6,8 @@ const computeECM = (req, res) => {
   const { test_pred } = req.body;
 
   const squareDiff = target_pred.map((el, index) => {
-    const diff = (test_pred - target_pred) ** 2;
-    console.log(diff);
+    const diff = (test_pred[index] - el) ** 2;
+    //console.log(diff);
     return diff;
     //return (el - (target_pred[index] - 0.1)) ** 2
   });
