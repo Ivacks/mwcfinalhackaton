871a20a9256590147876827ef481ea41d1032f0b
rovilram <robertovillaresrama@gmail.com>
2021-06-29 13:04:43 +0200

    computeECM endpoint

--- src/utils/computeECM.js
+++ src/utils/computeECM.js
@@ -3,10 +3,10 @@ const target_pred = require('../../Data-set/csvjson');
 
 const computeECM = (req, res) => {
 
-  const { test_pred } = req.body;
+  const { predictions } = req.body;
 
   const squareDiff = target_pred.map((el, index) => {
-    const diff = (test_pred[index] - el) ** 2;
+    const diff = (predictions[index] - el) ** 2;
     //console.log(diff);
     return diff;
     //return (el - (target_pred[index] - 0.1)) ** 2
